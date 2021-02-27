70fbfa2f06872038e6a7d55d52bd59e793bedc97
aditya <sadityar369@gmail.com>
2021-02-27 13:36:10 -0500

    payment processing sanction module

---
+++ src/main/java/com/hcl/payment/sanction/SanctionApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.payment.sanction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcl/payment/sanction/SanctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.payment.sanction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
